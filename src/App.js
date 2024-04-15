8293e34e0fa54b61b241467cb30c7ade6b8b855a
CC <763793173@qq.com>
2024-04-15 14:23:17 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Switch from "./components/Switch";
 
 function App() {
+  const [value, setValue] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="ParamsSwitch">
+          <p className="Params">Params</p>
+          <Switch
+            isOn={value}
+            onColor="#EF476F"
+            handleToggle={() => setValue(!value)}
+          />
+        </div>
       </header>
     </div>
   );
