1710a5c63b3448f6068c101fdafa767cca0114b9
CC <763793173@qq.com>
2024-04-15 19:02:50 +0800

    完成基础布局

--- src/App.js
+++ src/App.js
@@ -3,9 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import Switch from "./components/Switch";
 import GenerateActivity from "./components/GenerateActivity";
+import { BottomNavigation } from "reactjs-bottom-navigation";
+import happyIcon from "./img/happy.svg";
+import boredIcon from "./img/bored.svg";
+import collectSelectIcon from "./img/collect_select.svg";
+import collectUnSelectIcon from "./img/collect_unselect.svg";
 
 function App() {
-  const [value, setValue] = useState(false);
+  const [itemId, setItemId] = useState(0);
+  const bottomNavItems = [
+    {
+      title: "Generate",
+      onClick: ({ id }) => setItemId(id),
+      icon: <img style={styles.icon} src={boredIcon} alt="" />,
+      activeIcon: <img style={styles.icon} src={happyIcon} alt="" />,
+    },
+    {
+      title: "Collect",
+      onClick: ({ id }) => setItemId(id),
+      icon: <img style={styles.icon} src={collectUnSelectIcon} alt="" />,
+      activeIcon: <img style={styles.icon} src={collectSelectIcon} alt="" />,
+    },
+  ];
 
   return (
     <div className="App">
@@ -15,7 +34,14 @@ function App() {
         </div>
 
         <div className="MainContent">
-          <GenerateActivity />
+          {itemId == 0 ? <GenerateActivity /> : null}
+          <BottomNavigation
+            items={bottomNavItems}
+            selected={0}
+            onItemClick={(item) => console.log(item)}
+            activeBgColor="#4d6dde"
+            activeTextColor="white"
+          />
         </div>
       </header>
     </div>
@@ -23,3 +49,10 @@ function App() {
 }
 
 export default App;
+
+const styles = {
+  icon: {
+    width: "35px",
+    height: "35px",
+  },
+};
